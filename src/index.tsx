a1698171e5c46b7d08ac9a32f5851ce18d7381af
Maciek_dot <maciejd0@op.pl>
2021-07-24 22:43:49 +0200

    add env vars, dashboard, my location, city details

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+  }
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
