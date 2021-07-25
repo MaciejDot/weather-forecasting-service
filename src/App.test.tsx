4c160ea661a1ebf4265e7b6ecad3f8b58b026a3e
Maciek_dot <maciejd0@op.pl>
2021-07-25 15:06:27 +0200

    styling App tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 import '@testing-library/jest-dom';
 import { render, screen } from "@testing-library/react";
-import {MemoryRouter} from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 import App from "./App";
 
 test("App loads 404 on unknown route", async () => {
