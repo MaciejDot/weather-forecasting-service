87bdf905b2b13bcd03d7764257a6b1f39a4b0ce1
Maciek_dot <maciejd0@op.pl>
2021-07-25 14:49:58 +0200

    change nameless throw to Error object

--- src/fetch/fetchResourceThrowOnError.ts
+++ src/fetch/fetchResourceThrowOnError.ts
@@ -3,6 +3,6 @@ export const fetchResocureThrowOnError = (resocure: string) =>
     return fetch(resocure).then(res => {
         if(res.ok)
             return res.json()
-        throw {message: "api fetch was unsuccesfull"}
+        throw new Error("api fetch was unsuccesfull")
         })
 }
