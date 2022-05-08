98fcd49887c1e42ca6e701c71effe61fa6e37110
Maciek_dot <maciejd0@op.pl>
2022-05-08 15:15:07 +0200

    script up

--- .dependency-cruiser.js
+++ .dependency-cruiser.js
@@ -103,7 +103,9 @@ module.exports = {
         "This module depends on a module that cannot be found ('resolved to disk'). If it's an npm " +
         'module: add it to your package.json. In all other cases you likely already know what to do.',
       severity: 'error',
-      from: {},
+      from: {
+        pathNot: "src/react-app-env.d.ts",
+      },
       to: {
         couldNotResolve: true
       }
--- jest.config.ts
+++ jest.config.ts
@@ -8,6 +8,7 @@ const config: Config.InitialOptions = {
   testRegex: '__tests__/.*\\.(test|spec)\\.(tsx?)$',
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|less)$': 'identity-obj-proxy',
   },
 };
